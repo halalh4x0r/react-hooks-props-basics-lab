e84d988ad4d1a675c4f618dc639764c284f5e46d
halalh4x0r <mohamed.hadi@student.moringaschool.com>
2025-07-31 12:16:05 +0300

    modified 3 files and added a file from the 3

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({ bio, github, linkedin }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      
+      {/* Only show the bio if it exists */}
+      {bio && <p>{bio}</p>}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+
+      {/* Add the actual Links component */}
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
